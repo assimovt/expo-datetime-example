3707c91491425a29bec9bf6777cc9e57c1f2d2da
Tair Asim <tair@hey.com>
2021-10-18 14:39:18 +0200

    Basic usage of DateTimePicker

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { View, Button, Platform } from 'react-native'
+import DateTimePicker from '@react-native-community/datetimepicker'
 
-export default function App() {
+const App = () => {
+  const [show, setShow] = useState(false)
+
+  const onChange = () => {
+    setShow(Platform.OS === 'ios')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <View>
+        <Button onPress={() => setShow(true)} title="Show date picker!" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={new Date()}
+          mode="datetime"
+          display="default"
+          onChange={onChange}
+        />
+      )}
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
